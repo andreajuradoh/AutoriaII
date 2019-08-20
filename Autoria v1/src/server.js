0988ca4696d5b8677b0478635c61d9f7b773d072
Gloria Alcivar <sukkie_hye501@hotmail.com>
2019-08-19 21:09:38 -0500

    Add files via upload

---
+++ Autoria v1/src/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+const path = require ('path');
+const mongoose = require ('mongoose');
+const passport = require ('passport');
+const flash = require ('connect-flash');
+const morgan = require ('morgan');
+const cookieParser = require ('cookie-parser');
+const bodyParser = require ('body-parser');
+const session = require ('express-session');
+
+const { url } = require('./config/database');
+
+mongoose.connect(url, {
+    useNewUrlParser: true
+});
+
+require('./config/passport')(passport);
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(session({
+    secret: 'hola',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//routes
+require('./app/routes')(app, passport);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'imgs')));
+
+app.listen(app.get('port'), () => { 
+    console.log('server on port ');
+});
